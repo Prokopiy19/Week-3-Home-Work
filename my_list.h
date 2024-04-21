10a1989fb9fb87c915b2de7c844bb63ce1adb197
Prokopiy Danilov <danilovprokopiy@gmail.com>
2024-04-22 02:56:34 +0900

    Добавил copy и move конструкторы и операторы присваивания в MyList, исправил баги

--- my_list.h
+++ my_list.h
@@ -8,7 +8,8 @@
 #include <utility>
 
 struct ListNodeBase {
-    ListNodeBase *next = nullptr;
+    ListNodeBase *next;
+    ListNodeBase(ListNodeBase* p = nullptr) : next{p} {}
 };
 
 template <typename T>
@@ -24,12 +25,71 @@ public:
     using Iterator = IteratorTemplate<false>;
     using ConstIterator = IteratorTemplate<true>;
     
+    MyList() = default;
+    
+    MyList(const MyList& rhs) {
+        auto tail = before_begin();
+        for (const auto& element : rhs) {
+            tail = insert_after(tail, element);
+        } 
+    }
+    
+    void swap(MyList& with) {
+        std::swap(dummy, with.dummy);
+    }
+    
+    MyList& operator=(const MyList& rhs) {
+        if (this != &rhs) { // избегаем присваивание себе
+            // чтобы минимизировать количество аллокаций динамической памяти,
+            // мы переиспользуем уже существующие узлы
+            auto lit = before_begin();
+            auto rit = rhs.cbefore_begin();
+            while (std::next(lit) != end() && std::next(rit) != rhs.cend()) {
+                ++lit;
+                ++rit;
+                *lit = *rit;
+            }
+            ++rit;
+            if (rit != rhs.cend()) {
+                // на правом списке больше элементов, чем в нашей, поэтому нам нужно создать новые узлы
+                while (rit != rhs.cend()) {
+                    lit = insert_after(lit, *rit);
+                    ++rit;
+                }
+            }
+            else {
+                // размер нашего списка был больше правого, поэтому удаляем лишние узлы
+                while (std::next(lit) != end()) {
+                    erase_after(lit);
+                }
+            }
+        }
+        return *this;
+    }
+    
+    MyList(MyList&& rhs) {
+        dummy = rhs.dummy;
+        rhs.dummy.next = nullptr;
+    }
+    
+    MyList& operator=(MyList&& rhs) {
+        if (this != &rhs) { // избегаем присваивание себе
+            clear();
+            dummy = rhs.dummy;
+            rhs.dummy.next = nullptr;
+        }
+        return *this;
+    }
+    
     ~MyList() {
-        auto ptr = static_cast<ListNode*>(dummy.next);
-        while (ptr != nullptr) {
-            auto next = static_cast<ListNode*>(ptr->next);
-            delete ptr;
-            ptr = next;
+        clear();
+    }
+    
+    bool empty() const { return begin() == end(); }
+    
+    void clear() {
+        while (!empty()) {
+            pop_front();
         }
     }
     
@@ -51,8 +111,8 @@ public:
         if (pos == cend()) {
             throw std::logic_error("MyList::insert_after: invalid position");
         }
-        auto next = pos.ptr->next;
-        pos.ptr->next = new ListNode{std::move(value), next};
+        auto tmp = pos.ptr->next;
+        pos.ptr->next = new ListNode{std::move(value), tmp};
         return Iterator{pos.ptr->next};        
     }
     
@@ -62,13 +122,31 @@ public:
     }
     
     Iterator erase_after(ConstIterator pos) {
-        if (pos == cend() || pos.ptr->next == nullptr) {
+        if (pos == cend() || std::next(pos) == cend()) {
             throw std::logic_error("MyList::erase_after: invalid position");
         }
-        auto next = pos.ptr->next->next;
-        delete pos.ptr->next;
-        pos.ptr->next = next;
-        return Iterator{next};
+        auto tmp = pos.ptr->next->next;
+        delete static_cast<ListNode*>(pos.ptr->next);
+        pos.ptr->next = tmp;
+        return Iterator{tmp};
+    }
+    
+    // удаление всех узлов со значением value
+    int erase_by_value(const T& value) {
+        int erased_count = 0;
+        auto it = before_begin();
+        auto next = std::next(it);
+        while (next != end()) {
+            if (*next == value) {
+                ++erased_count;
+                next = erase_after(it);
+            }
+            else {
+                ++it;
+                ++next;
+            }
+        }
+        return erased_count;
     }
     
     Iterator push_front(const T& value) { return insert_after(before_begin(), value); }
@@ -76,7 +154,7 @@ public:
     
     Iterator pop_front() { return erase_after(before_begin()); }
     
-    Iterator find(const T& query) { return std::find(begin(), end(), query); }
+    Iterator find(const T& query, ConstIterator pos = ConstIterator{nullptr}) { return std::find(begin(), end(), query); }
     ConstIterator find(const T& query) const { return std::find(cbegin(), cend(), query); }
 };
 
@@ -104,7 +182,7 @@ public:
     using pointer           = typename std::conditional<IsConst, const T*, T*>::type;
     using reference         = typename std::conditional<IsConst, const T&, T&>::type;
 
-    explicit IteratorTemplate(ListNodeBase* p) : ptr{p} {}
+    explicit IteratorTemplate(const ListNodeBase* p) : ptr{const_cast<ListNodeBase*>(p)} {}
     
     // имплицитное приведение мутабельного итератора к константному итератору
     IteratorTemplate(const IteratorTemplate<false>& rhs) : ptr{rhs.ptr} {}
